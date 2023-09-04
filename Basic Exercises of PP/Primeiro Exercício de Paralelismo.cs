98ce3a575b080d3b528ba99fd6c9e988734743bc
Matheus Veras <99488005+MrVeGGi3@users.noreply.github.com>
2023-09-04 17:14:57 -0300

    FirstExercise.cs

--- Basic Exercises of PP/Primeiro Exercício de Paralelismo.cs
+++ Basic Exercises of PP/Primeiro Exercício de Paralelismo.cs
@@ -1,41 +1,38 @@
 /******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
+Parallelizing one verification in the (X,Y) axis of one video game,
+To see if exists two objects overlaying each other.
 
 *******************************************************************************/
 using System;
-using System.Threading;
+using System.Threading; 
 
 class HelloWorld {
-  public static int[] VetX = new int [4];
-  public static int[] VetY = new int [4];
+  public static int[] VetX = new int [4]; //declaring VetX as point positions of the objects(four);
+  public static int[] VetY = new int [4]; //declaring VetY as a point positions of the objects(four);
   static void Main() {
     Random rnd = new Random();
-    Thread t1 = new Thread(Calc1);
-    Thread t2 = new Thread(Calc2);
+    Thread t1 = new Thread(Calc1); //instantiate the first thread;
+    t1.Start(); //Start the first Thread;
+    Thread t2 = new Thread(Calc2);//instantiate the second thread;
+    t2.Start();//Start the second Thread;
     for(int i = 0; i < VetX.Length; i++){
-        VetX[i] = rnd.Next(0,4);
-        Console.WriteLine("VetX {0} = {1}", i, VetX[i]);
+        VetX[i] = rnd.Next(0,255); 
+        Console.WriteLine("VetX {0} = {1}", i, VetX[i //positions generator of X;
     }
     for(int i = 0; i < VetY.Length; i++){
-        VetY[i] = rnd.Next(0,4);
-        Console.WriteLine("VetY {0} = {1}", i, VetY[i]);
+        VetY[i] = rnd.Next(0,255);
+        Console.WriteLine("VetY {0} = {1}", i, VetY[i]); //positions generator of Y;
     }
-    t1.Start();
-    t2.Start();
-    t1.Join();
-    t2.Join();
+    t1.Join(); //block thread
+    t2.Join();//block thread
   }
   static void Calc1()
     {
        int i = 0;
        while( i < VetX.Length){
             for(int n = 1; n<VetX.Length/2; n++){
-                if(VetX[i] == VetX[i+n] && VetY[i] == VetY[i+n]){
-                     Console.WriteLine("Há sobreposição nos pontos: {0} e {1}", i, i+n);
+                if(VetX[i] == VetX[i+n] && VetY[i] == VetY[i+n]){ //checks if (X1, Y1) = (X2,Y2) in the first half
+                     Console.WriteLine("Overlay in the Points: {0} e {1}", i, i+n);
                 }  
             }
             i++;
@@ -46,8 +43,8 @@ class HelloWorld {
        int i = 0;
        while(i < VetY.Length){
             for(int n = VetX.Length/2; n<VetY.Length; n++){
-                if(VetX[i] == VetX[i+n] && VetY[i] == VetY[i+n]){
-                    Console.WriteLine("Há sobreposição nos pontos: {0} e {1}", i, i+n);
+                if(VetX[i] == VetX[i+n] && VetY[i] == VetY[i+n]){//checks if (X1, Y1) = (X2,Y2) in the second half
+                    Console.WriteLine("Overlay in the points: {0} e {1}", i, i+n);
                 }  
             }
             i++;
